672da85913a7520db0dad1faa24737dda8baffd0
Jose Ramos <jmrp.04@gmail.com>
2023-03-08 23:29:39 -0500

    Working on the DatePicker of the Personal Expenses app.

--- Section 4/expenses_tracker/lib/widgets/new_transaction.dart
+++ Section 4/expenses_tracker/lib/widgets/new_transaction.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -15,72 +16,101 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
-
-  final amountController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
   void _submitData() {
-    final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
 
     if (enteredAmount <= 0 || enteredTitle.isEmpty) {
       return;
     }
 
     widget.addTx(
-      titleController.text,
-      double.parse(amountController.text),
+      _titleController.text,
+      double.parse(_amountController.text),
     );
 
     Navigator.of(context).pop();
   }
 
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2022),
+      lastDate: DateTime.now(),
+    ).then((pickerDate) {
+      if (pickerDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickerDate;
+      });
+    });
+    print('...');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Title',
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Title',
+              ),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData,
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
             ),
-            controller: titleController,
-            onSubmitted: (_) => _submitData,
-            // onChanged: (val) {
-            //   titleInput = val;
-            // },
-          ),
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Amount',
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Amount',
+              ),
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submitData,
+              // onChanged: (val) {
+              //   amountInput = val;
+              // },
             ),
-            controller: amountController,
-            keyboardType: TextInputType.number,
-            onSubmitted: (_) => _submitData,
-            // onChanged: (val) {
-            //   amountInput = val;
-            // },
-          ),
-          Row(
-            children: [
-              const Text('No Date Chosen!'),
-              TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Chose Date',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ))
-            ],
-          ),
-          ElevatedButton(
-            onPressed: _submitData,
-            child: Text('Add Transaction'),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Theme.of(context).primaryColor,
+            Container(
+              height: 70,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date: ${DateFormat.yMd().format(_selectedDate as DateTime)}',
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: _presentDatePicker,
+                      child: const Text(
+                        'Chose Date',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ))
+                ],
+              ),
             ),
-          ),
-        ],
+            ElevatedButton(
+              onPressed: _submitData,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+              ),
+              child: const Text('Add Transaction'),
+            ),
+          ],
+        ),
       ),
     );
   }
